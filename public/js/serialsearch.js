dc371f01d7e52736c0d04b4cd062bbe61cb441ae
0xRemyRuiz <remyruiz@protonmail.com>
2026-03-20 10:37:48 +0100

    fixed the javascript bug for the search id

--- public/js/serialsearch.js
+++ public/js/serialsearch.js
@@ -172,9 +172,8 @@ function getById(id) {
     async function searchIds() {
         for (var i = item_list.length - 1; i >= 0; i--) {
             try {
-                const id = item_list[i];
                 const res  = await fetch(
-                    `${AJAX_URL}?id=${encodeURIComponent(id)}`,
+                    `${AJAX_URL}?id=${item_list[i].id}`,
                     { credentials: 'same-origin' }
                 );
                 if (!res.ok) throw new Error('HTTP ' + res.status);
